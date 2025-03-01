d10319fdaf40c82d01449145be1501c8b01607c8
hwwiebe99 <91705228+hwwiebe99@users.noreply.github.com>
2025-03-01 14:08:13 -0800

    Add files via upload

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,28 +1,33 @@
 var counter2;
 var counter;
 var pos = 40;
-function myMove () {
-    counter = 0
-    counter2 = 1;
+var headerpos = 1883;
 
-    elem = document.getElementById("side-drop");
+function myMove () {
+    counter = 0;
+
+    elem_holder = document.getElementById("holder");
+    elem_header = document.getElementById("container");
     
-    if (counter == 0 && counter2 == 1)
+    if (counter == 0)
     {
         id = setInterval(frame, 1);
         function frame() {
-            if (counter == 0 && counter2 == 1)
+            if (counter == 0)
             {
-                if(pos == 150)
+                if(pos >= 150)
                 {
                     clearInterval(id);
                 }
                 else{
             
                     pos++;
-                    pos++;
-                    elem.style.width = pos + 'px';
+                    headerpos--;
+                    elem_holder.style.width = pos + 'px';
+                    elem_header.style.width = headerpos + 'px';
                 }
+
+                
             }
             else
             {
@@ -32,30 +37,36 @@ function myMove () {
         }
 
     }
-    return counter2;
 }
 
 function leaveMove() {
-    counter2 = 0;
     counter = 1;
-    elem = document.getElementById("side-drop");
-    if(counter2 == 0 && counter == 1)
+    elem = document.getElementById("holder");
+    elem_header = document.getElementById("container")
+    if(counter == 1)
     {
         var id = setInterval(frame2, 1);
         function frame2 ()
         {
-            if(counter == 1 && counter2 == 0)
+            if(counter == 1)
             {
-                if(pos == 40)
+                if(pos <= 40)
                 {
                     clearInterval(id);
                 }
                 else
                 {
                     pos--;
+                    headerpos++;
                     elem.style.width = pos + 'px';
+                    elem_header.style.width = headerpos + 'px';
                 }
 
+
+            }
+            else
+            {
+                clearInterval(id);
             }
         }
     }
